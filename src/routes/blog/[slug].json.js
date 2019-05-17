dd1c8bb99fd5da2feb14e82ee14f2f8b166e26a9
Donovan Hutchinson <donovanh@gmail.com>
2019-05-17 16:11:35 +0100

    Initial files

---
+++ src/routes/blog/[slug].json.js
@@ -0,0 +1,25 @@
+import send from "@polka/send";
+import get_posts from "./_posts.js";
+
+let lookup;
+
+export function get(req, res) {
+  try {
+    if (!lookup || process.env.NODE_ENV !== "production") {
+      lookup = new Map();
+      get_posts().forEach(post => {
+        lookup.set(post.slug, JSON.stringify(post));
+      });
+    }
+    const post = lookup.get(req.params.slug);
+
+    if (post) {
+      res.setHeader("Cache-Control", `max-age=${5 * 60 * 1e3}`); // 5 minutes
+      send(res, 200, post);
+    } else {
+      send(res, 404, { message: "not found" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
---
+++ src/utils/markdown.js
@@ -0,0 +1,41 @@
+export function extract_frontmatter(markdown) {
+	const match = /---\r?\n([\s\S]+?)\r?\n---/.exec(markdown);
+	const frontMatter = match[1];
+	const content = markdown.slice(match[0].length);
+
+	const metadata = {};
+	frontMatter.split('\n').forEach(pair => {
+		const colonIndex = pair.indexOf(':');
+		metadata[pair.slice(0, colonIndex).trim()] = pair
+			.slice(colonIndex + 1)
+			.trim();
+	});
+
+	return { metadata, content };
+}
+
+// map lang to prism-language-attr
+export const langs = {
+	bash: 'bash',
+	html: 'markup',
+	sv: 'markup',
+	js: 'javascript',
+	css: 'css'
+};
+
+
+// links renderer
+export function link_renderer (href, title, text) {
+	let target_attr = '';
+	let title_attr = '';
+
+	if (href.startsWith("http")) {
+		target_attr = ' target="_blank"';
+	}
+
+	if (title !== null) {
+		title_attr = ` title="${title}"`;
+	}
+
+	return `<a href="${href}"${target_attr}${title_attr}>${text}</a>`;
+}
